85a6cf9cfb899386f4ad1857602e03d19fb825ba
Nitesh Kumar <ns400882@gmail.com>
2022-12-06 17:19:36 +0530

    BugFixed

--- problem1.js
+++ problem1.js
@@ -1,9 +1,12 @@
 
 
 let prime_number=17;
-
-if(prime_number%2!=0){
-
+let var count=0;
+for (let i=1;i<=prime_number;i++){
+        if(prime_number%i==0){
+         count++
+}
+}if (count==2){
    console.log("Prime No");
 }
 else{
