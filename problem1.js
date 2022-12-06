51ca1b83e441e0d0fdba2802d67cabf68ba8380e
unknown <sanjurathaur99@gmail.com>
2022-12-06 15:14:16 +0530

    palidrome

--- problem1.js
+++ problem1.js
@@ -2,7 +2,7 @@
 
 let prime_number=17;
 
-if(prime_number%2==0){
+if(prime_number%2!=0){
 
    console.log("Prime No");
 }
---
+++ problem2.js
@@ -0,0 +1,10 @@
+
+
+
+let str="madam";
+let x="";
+for(let i=str.length-1>;i>=0;i--){
+
+  x+=str[i];
+
+}
